ce83c8132c624c22b54b6905ac174802ba92d568
Merkle Groot <merklegroot@gmail.com>
2026-04-11 16:20:38 -0400

    chain mail image

--- Tav.Tests/FightMonsterPortraitPanelBuilderTests.cs
+++ Tav.Tests/FightMonsterPortraitPanelBuilderTests.cs
@@ -33,11 +33,11 @@ public class FightMonsterPortraitPanelBuilderTests(ITestOutputHelper outputHelpe
         panel[0].ShouldBe(@"┌──────────────┐");
         panel[1].ShouldBe(@"│    5/6 HP    │");
         panel[2].ShouldBe(@"│              │");
-        panel[3].ShouldBe(@"│    .-----.   │");
-        panel[4].ShouldBe(@"│    o    o    │");
-        panel[5].ShouldBe(@"│    \  ^  /   │");
-        panel[6].ShouldBe(@"│     [===]    │");
-        panel[7].ShouldBe(@"│   /       \s │");
+        panel[3].ShouldBe(@"│   .-----.    │");
+        panel[4].ShouldBe(@"│   o    o     │");
+        panel[5].ShouldBe(@"│   \  ^  /    │");
+        panel[6].ShouldBe(@"│    [===]     │");
+        panel[7].ShouldBe(@"│  /       \s  │");
         panel[8].ShouldBe(@"│              │");
         panel[9].ShouldBe(@"│ Bone Gnawer  │");
         panel[10].ShouldBe(@"└──────────────┘");
